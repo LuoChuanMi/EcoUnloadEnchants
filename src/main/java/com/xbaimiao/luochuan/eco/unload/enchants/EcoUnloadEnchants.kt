cdb0dc73a41080e085baad42003b2faa6ab7f0fb
小白 <3104026189@qq.com>
2023-01-11 22:51:39 +0800

    fix bugs

--- src/main/java/com/xbaimiao/luochuan/eco/unload/enchants/EcoUnloadEnchants.kt
+++ src/main/java/com/xbaimiao/luochuan/eco/unload/enchants/EcoUnloadEnchants.kt
@@ -2,9 +2,13 @@ package com.xbaimiao.luochuan.eco.unload.enchants
 
 import com.xbaimiao.easylib.sendLang
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractEvent
 import top.mcplugin.lib.Plugin
 
 @Suppress("unused")
@@ -16,6 +20,18 @@ class EcoUnloadEnchants : Plugin() {
 
     override fun enable() {
         saveDefaultConfig()
+        Bukkit.getPluginManager().registerEvents(object : Listener {
+            val blocks = config.getStringList("blocks").map { it.toLocation() }
+
+            @EventHandler
+            fun click(event: PlayerInteractEvent) {
+                val block = event.clickedBlock ?: return
+                if (blocks.any { it.blockZ == block.z && it.blockX == block.x && it.blockY == block.y }) {
+                    event.isCancelled = true
+                }
+            }
+
+        }, this)
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
@@ -31,6 +47,16 @@ class EcoUnloadEnchants : Plugin() {
             GUI.open(player)
         }
         return true
+    }
+
+    private fun String.toLocation(): Location {
+        val split = this.split("/")
+        return Location(
+            Bukkit.getWorld(split[0]),
+            split[1].toDouble(),
+            split[2].toDouble(),
+            split[3].toDouble()
+        )
     }
 
 }
